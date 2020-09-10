526145fc370d2af8ff216d43f281882888e29d34
heorhii <georgeovod@gmail.com>
2020-09-10 10:44:18 +0200

    added Makefile, gitignore, greeting

---
+++ bin/brain-games.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import {greeting} from '../src/cli.js';
+import {userName} from '../src/cli.js';
+console.log(`Hello, ${userName}!`);
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+let greeting = console.log("Welcome to the Brain Games!");
+export {greeting};
+let userName = readlineSync.question('May I have your name?');
+export {userName};

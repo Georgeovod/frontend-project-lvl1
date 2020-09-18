c922cfce5b5bf9d1c5bad42d4470f4d714990b5f
heorhii <georgeovod@gmail.com>
2020-09-18 10:45:33 +0200

    update bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import {greeting} from '../src/cli.js';
-import {userName} from '../src/cli.js';
+// eslint-disable-next-line no-unused-vars
+import { greeting, userName } from '../src/cli.js';
+
 console.log(`Hello, ${userName}!`);
